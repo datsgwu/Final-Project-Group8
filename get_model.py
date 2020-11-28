0e87a5a08feabdbdc8860e4333a92169a4c0dc3b
Thomas Keeley <thomaskeeley@Thomass-MacBook-Pro.local>
2020-11-28 16:37:52 -0500

    Modified directory paths

--- get_model.py
+++ get_model.py
@@ -8,13 +8,13 @@ Created on Sun Nov 15 12:24:59 2020
 
 import os
 
-wd = '/Project/'
+wd = '/home/ubuntu/project/'
 
 # path where pre-trained weights should be saved
 model_dir = 'keras-retinanet/snapshots/'
 # model url
 model_url = 'https://github.com/fizyr/keras-retinanet/releases/download/0.5.1/resnet50_coco_best_v2.1.0.h5'
-# retrieve model from model url and save 
 
+# retrieve model from model url and save
 os.chdir(wd + model_dir)
 os.system('wget ' + model_url)
